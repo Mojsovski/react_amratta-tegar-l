05a4d67194e45f8d1de4e639bd20401c761ac175
Mojsovski <amrattategar30@gmail.com>
2023-09-19 16:35:46 +0700

    update commit tugas React Fundamental

--- 11_React-Fundamental/praktikum/create-product/src/page/CreateProduct.jsx
+++ 11_React-Fundamental/praktikum/create-product/src/page/CreateProduct.jsx
@@ -203,6 +203,24 @@ export default function CreateProduct() {
           </div>
         </div>
       </form>
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-10">
+          <h3>List Product:</h3>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Product Name</th>
+                <th>Product Category</th>
+                <th>Product Freshness</th>
+                <th>Description</th>
+                <th>Product Price</th>
+              </tr>
+            </thead>
+            <tbody id="productTableBody"></tbody>
+          </table>
+        </div>
+      </div>
       <br />
     </>
   );
